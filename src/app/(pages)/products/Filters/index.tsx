d4283126d2ab21007814dbc7fb472fb59a676179
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-01-07 18:08:50 +0200

    Added Filter and Check box component

---
+++ src/app/(pages)/products/Filters/index.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import React from 'react'
+
+import { Category } from '../../../../payload/payload-types'
+import { Checkbox } from '../../../_components/Checkbox'
+import { useFilter } from '../../../_providers/Filter'
+
+import classes from './index.module.scss'
+
+const Filters = ({ categories }: { categories: Category[] }) => {
+    const { categoryFilters, setCategoryFilters } = useFilter()
+
+    const handleCategories = (categoryId: string) => {
+        if (categoryFilters.includes(categoryId)) {
+            const updatedCategories = categoryFilters.filter(id => id !== categoryId)
+
+            setCategoryFilters(updatedCategories)
+        } else {
+            setCategoryFilters([...categoryFilters, categoryId])
+        }
+    }
+
+    return (
+        <div className={classes.filters}>
+            <div>
+                <h6 className={classes.title}>Product Categories</h6>
+                <div className={classes.categories}>
+                    {categories.map(category => {
+                        const isSelected = categoryFilters.includes(category.id)
+
+                        return (
+                            <Checkbox
+                                key={category.id}
+                                label={category.title}
+                                value={category.id}
+                                isSelected={isSelected}
+                                onClickHandler={handleCategories}
+                            />
+                        )
+                    })}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Filters
